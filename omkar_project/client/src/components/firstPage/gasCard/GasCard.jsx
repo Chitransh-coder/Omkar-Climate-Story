7354e0d4180420d2bb6e447184ea147564aabad1
DevyanshuNegi <devyanshunegi@gmail.com>
2024-10-06 23:02:59 +0530

    added image

--- omkar_project/client/src/components/firstPage/gasCard/GasCard.jsx
+++ omkar_project/client/src/components/firstPage/gasCard/GasCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 // import Card from "./Card";
+import hh from "../../../assets/p1.png";
 export default function GasCard() {
   
   function Card({name, imageUrl, description}) {
@@ -41,7 +42,7 @@ export default function GasCard() {
     {
       id: 1,
       name: "Carbon Dioxide (CO₂)",
-      imageUrl: "/api/placeholder/400/300",
+      imageUrl: hh,
       description:
         "CO₂ is the most well-known greenhouse gas. It's released primarily from burning fossil fuels (like coal, oil, and natural gas) and deforestation. CO₂ traps heat in the atmosphere, contributing significantly to global warming.",
     },
